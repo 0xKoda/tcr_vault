ca99c27bd47fb335248754fad296051d73bcf2cc
0xkoda <koda_0x@protonmail.com>
2022-02-07 14:40:35 +1000

    add fToken rari mint/redeem

--- src/boostedTCR.sol
+++ src/boostedTCR.sol
@@ -3,17 +3,25 @@ pragma solidity ^0.8.0;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {ERC4626} from "solmate/mixins/ERC4626.sol";
 
+interface IFERC20 {
+    function mint(uint256 mintAmount) external returns (uint256);
+
+    function redeem(uint256 redeemTokens) external returns (uint256);
+}
+
 abstract contract boostedTCR is ERC20, ERC4626 {
     //the underlying token the vault accepts
     ERC20 public immutable UNDERLYING;
-    ERC20 public immutable fToken;
+    IFERC20 public immutable fToken;
 
     mapping(address => uint256) balances;
 
-    constructor(ERC20 underlying) ERC4626(underlying, "bTCR", "bTCR") {
-        UNDERLYING = ERC20(underlying);
+    constructor(ERC20 underlying, IFERC20 _fERC20)
+        ERC4626(underlying, "bTCR", "bTCR")
+    {
+        UNDERLYING = underlying;
         // todo set fToken addr after pool creation
-        fToken = ERC20(0x0...);
+        fToken = _fERC20;
     }
 
     function afterDeposit(uint256 amount, uint256 shares) internal override {
@@ -22,7 +30,7 @@ abstract contract boostedTCR is ERC20, ERC4626 {
         balances[msg.sender] += amount;
     }
 
-    function beforeWithdrawal(uint256 amount, uint256 shares) internal override {
+    function beforeWithdraw(uint256 amount, uint256 shares) internal override {
         assert(balances[msg.sender] >= amount);
         balances[msg.sender] -= amount;
         require(fToken.redeem(amount) == 0, "something went wrong");
